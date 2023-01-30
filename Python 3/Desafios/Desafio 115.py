4e42aa485be523a84221b2c948988f163e1b7329
WandersonK <wan01dez0013@gmail.com>
2023-01-30 10:50:41 -0300

    Fim do Mundo 3

--- Python 3/Desafios/Desafio 115.py
+++ Python 3/Desafios/Desafio 115.py
@@ -6,7 +6,8 @@ from ex115.consultar import consulta
 from ex115.menu import menu
 from ex115.verifica_valido import verificar
 
-base_dados = 'C:/Users/wan01/Documents/Curso_em_Video/Python 3/Desafios/ex115/base_dados.txt'
+# base_dados = 'C:/Users/wan01/Documents/Curso_em_Video/Python 3/Desafios/ex115/base_dados.txt'
+base_dados = '/home/wanderson/Documentos/Curso_em_Video/Python 3/Desafios/ex115/base_dados.txt'
 
 
 while True:
@@ -21,4 +22,33 @@ while True:
         if opcao == 3:
             print('== Até Logo! ==')
             break
+
+
+
+# Forma do Guanabara
+
+from ex115.forma_guanabara.lib.interface import *
+from ex115.forma_guanabara.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu_g(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        lerArquivo(arq)
+    elif resposta == 2:
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema... até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
     
